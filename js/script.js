a4bce8d8855005b446d1bfd687867a529e657c4d
Anya Lobko <anya.lobko@gmail.com>
2021-08-12 21:26:07 +0200

    objects created

--- js/script.js
+++ js/script.js
@@ -3,3 +3,21 @@ const question1 = {
   answer: '20/08/2021',
   correctAnswer: '12/08/2021'
 }
+
+const question2 = {
+  question: 'What day is today?',
+  answer: 'Monday',
+  correctAnswer: 'Thursday'
+}
+
+const question3 = {
+  question: 'What city is the capital of Brasil?',
+  answer: 'Rio',
+  correctAnswer: 'Brasilia'
+}
+
+const question4 = {
+  question: 'When was the last FIFA championshit',
+  answer: 'in 2018',
+  correctAnswer: 'in 2018'
+}
