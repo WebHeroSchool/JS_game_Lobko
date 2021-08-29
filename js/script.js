bc921bf96bf19ab64f07f633b47bba4ed9738545
Anya Lobko <anya.lobko@gmail.com>
2021-08-29 11:58:42 +0200

    task 6 fixed

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 const q1 = {
-  question: 'What is the date today?',
+  question: '9 + 10 - 5 = ?',
   answer: {
-    a: 'a) July 2nd',
-    b: 'b) July 4th',
-    c: 'c) August 15th'
+    a: 'a) 15 ',
+    b: 'b) 11 ',
+    c: 'c) 14 '
   },
   correctAnswer: 'c'
 }
 
 const q2 = {
-  question: 'What day is today?',
+  question: '(3 + 7) / 5 + 6 = ?',
   answer: {
-    a: 'a) Monday',
-    b: 'b) Friday',
-    c: 'c) Sunday'
+    a: 'a) 11 ',
+    b: 'b) 8 ',
+    c: 'c) 5 '
   },
   correctAnswer: 'b'
 }
@@ -21,19 +21,19 @@ const q2 = {
 const q3 = {
   question: '2 + 2 = ?',
   answer: {
-    a: 'a) 7',
-    b: 'b) 5',
-    c: 'c) 4'
+    a: 'a) 7 ',
+    b: 'b) 5 ',
+    c: 'c) 4 '
   },
   correctAnswer: 'c'
 }
 
 const q4 = {
-  question: 'When was the last FIFA championship?',
+  question: '8 - 4 / 2 + 6 = ?',
   answer: {
-    a: 'a) in 2018',
-    b: 'b) in 2017',
-    c: 'c) this year'
+    a: 'a) 12 ',
+    b: 'b) 10 ',
+    c: 'c) 15 '
   },
   correctAnswer: 'a'
 }
@@ -44,14 +44,16 @@ let i, correctAnswer = 0;
 
 let userAnswer = [];
 
+//alert the start of the game
 alert('Посчитаем вместе!');
 
+//display questions and ask for answers
 for (i = 0; i < arr.length; i++) {
-
   let a = prompt(arr[i].question + '\n' + Object.values(arr[i].answer));
   userAnswer.push(a);
 }
 
+//function for sorting 
 function checkAnswer(a) {
   for (i = 0; i < a.length; i++) {
     if (a[i] === arr[i].correctAnswer) {
@@ -60,7 +62,9 @@ function checkAnswer(a) {
   }
 }
 
+//call the checkAnswer function
 checkAnswer(userAnswer);
 
+//print to HTML the amount of correct answers
 let p = document.getElementById('answer');
 p.innerHTML = ('Классно поиграли! У тебя ' + correctAnswer + ' правильных ответa.');
